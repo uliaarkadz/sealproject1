d7f6950bc1dcbcc5b433e35c8770152d98377870
ybuiko <juliabuiko@gmail.com>
2023-12-04 23:15:02 -0500

    added select handling and search handling, function to get obj by search eandpoint

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 //VARIABLES
 
-//let museumDepartments = [];
-const artObjectIds = [];
 const baseURL = "https://collectionapi.metmuseum.org/public/collection/v1/";
 
 //FUNCTIONS
@@ -21,6 +19,7 @@ function getDepartments(parameter) {
 //function to get artwork by department id
 function getArtIdsByDepartment(parameter, departmentId) {
   const url = `${baseURL}${parameter}?departmentIds=${departmentId}`;
+  const artItem = {};
   fetch(url)
     .then((res) => {
       return res.json();
@@ -28,20 +27,37 @@ function getArtIdsByDepartment(parameter, departmentId) {
     .then((data) => {
       const arr = data.objectIDs;
       const slicedArray = arr.slice(0, 50);
-      displayDepartmentArtObject(slicedArray);
+      slicedArray.forEach((i) => {
+        return fetch(`${baseURL}objects/${i}`)
+          .then((res) => {
+            return res.json();
+          })
+          .then((data) => {
+            displayArtObject(data);
+          });
+      });
     });
 }
 
 //function get artwork info by objectid
-
-function getArtOblectInfo(parameter, objectId) {
-  const url = `${baseURL}${parameter}/${objectId}`;
+function getArtObjectsBySearch(parameter, search) {
+  const url = `${baseURL}${parameter}?q=${search}&hasImages=true`;
   fetch(url)
     .then((res) => {
       return res.json();
     })
     .then((data) => {
       console.log(data);
+
+      data.objectIDs.forEach((i) => {
+        return fetch(`${baseURL}objects/${i}`)
+          .then((res) => {
+            return res.json();
+          })
+          .then((data) => {
+            displayArtObject(data);
+          });
+      });
     });
 }
 
@@ -49,6 +65,7 @@ function getArtOblectInfo(parameter, objectId) {
 function addSelectOptions(options) {
   const $departments = $("#departments");
   const $select = $("<select>");
+  $select.on("change", handleSelect);
   $departments.append($select);
   options.forEach((item) => {
     $select.append(
@@ -57,28 +74,57 @@ function addSelectOptions(options) {
     );
   });
 }
-//function to display art object by department
 
-function displayDepartmentArtObject(options) {
+//function to display art object by department
+function displayArtObject(options) {
   const $maincontainer = $("#maincontainer");
   const $artObjects = $("<div>").addClass("artObjects");
   $maincontainer.append($artObjects);
-  options.forEach((item) => {
-    $artObjects.append(`<div class=art id =${item}></div></div>`);
-  });
-  artWorkDetail();
+  $artObjects.append(
+    `<div class=art id=${options.objectID}>
+    <div> ${options.title}</div>
+    <div>${options.artistDisplayName}</div>
+    <div> <img src='${options.primaryImageSmall}'></div></div>`
+  );
 }
 
-//function get object info
-function artWorkDetail(itemData) {
-  const $divArtItem = $(".art");
-  var ids = $divArtItem.map(function () {
-    return $(this).attr("id");
-  });
-  ids.forEach((i) => {});
+//function to display art object full info
+function displayArtObjectFull(options) {
+  const $maincontainer = $("#maincontainer");
+  const $artObjects = $("<div>").addClass("artObjects");
+  $maincontainer.append($artObjects);
+  $artObjects.append(
+    `<div class=art id=${options.objectID}>
+      <div> ${options.title}</div>
+      <div>${options.artistDisplayName}</div>
+      <div> <img src='${options.primaryImageSmall}'></div></div>`
+  );
 }
+
 //MAIN CODE
 getDepartments("departments");
-getArtIdsByDepartment("objects", 1);
 
-//displayDepartmentArtObject();
+//getArtObjectsBySearch("search", "sunflowers");
+
+// function to handle the select
+function handleSelect(event) {
+  //get current select values
+  const currentValue = event.target.value;
+  //fetch the objects
+  getArtIdsByDepartment("objects", currentValue);
+}
+// function to handle the form submission
+function handleSubmit(event) {
+  //prevent refreshing of the page from the form submision
+  event.preventDefault();
+  // grab the form from the event
+  const form = event.target;
+  //create form data obj to access form data
+  const formData = new FormData(form);
+  //grab the serach param
+  const search = formData.get("search");
+  //fetch the search results
+  getArtObjectsBySearch("search", "search");
+}
+
+document.querySelector("form").addEventListener("submit", handleSubmit);
